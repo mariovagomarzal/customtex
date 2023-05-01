55a910a618dc01c4b8f113f78fafcd5a7879312d
Mario Vago Marzal <mariovagomarzal@gmail.com>
2023-05-01 15:21:30 +0200

    Initial commit for the cookiecutter version

--- customtex/__init__.py
+++
@@ -1,9 +0,0 @@
-"""CustomTeX is a command line utility for setting 
-up LaTeX projectsbased on customizable templates."""
-
-__version__ = "1.0.0"
-
-from customtex.configs import *
-from customtex.texts import *
-from customtex.templates import *
-from customtex.parser import *
--- customtex/defaults.py
+++
@@ -1,7 +0,0 @@
-"""This module contains the default values for the customtex package."""
-
-from pathlib import Path
-
-
-CUSTOMTEX_DIR = Path.home() / ".customtex"
-TEMPLATE_EXTENSION = ".ctex"
